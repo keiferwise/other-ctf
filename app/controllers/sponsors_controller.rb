bf0a63fc164f60b56a3aeb4edd818e4c5530418c
Keifer Wiseman <keiferwiseman@gmail.com>
2020-11-15 20:10:46 -0500

    adding sponsor stuff and come other stuff

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,5 +1,44 @@
 class SponsorsController < ApplicationController
-    def manage
-       render 'index' 
+    def index
+        @sponsors = Sponsor.all 
     end
+    def show
+        @sponsor = Sponsor.find(params[:id])
+    end
+    def edit
+        require_staff
+        @person = Sponsor.find(params[:id])
+    end
+    def new
+        require_staff
+        @sponsor = Sponsor.new
+    end
+    def create
+        require_staff
+        @sponsor = Sponsor.new(sponsor_params)
+        if @sponsor.save 
+          redirect_to @sponsor
+        else
+          render 'new'
+        end
+    end
+    def manage
+        require_staff
+        @sponsors = Sponsor.all
+    end
+    def update
+        require_staff
+        @sponsor = Sponsor.find(params[:id])
+       
+        if @sponsor.update(sponsor_params)
+          redirect_to @sponsor
+        else
+          render 'edit'
+        end
+      end
+  #
+  private
+  def sponsor_params
+    params.require(:sponsor).permit(:title, :slogan, :about, :link)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   #challenges 
   post '/challenges/:id/submit',to: 'challenges#submit_flag', as: 'submit'
   get '/challenges/:id/hint', to:'challenges#get_hint', as: 'hint'
+  #score board
+  get '/scoreboard' => 'teams#index'
   #admin teams
   get '/admin/teams'      => 'teams#manage'
   get '/admin/teams/new'  => 'teams#new'
@@ -34,7 +36,7 @@ Rails.application.routes.draw do
   resource :user_session
   resources :users,       only: [:index, :show, :create, :update, :edit]
   resources :challenges,  only: [:index, :show, :create, :update, :edit]
-  resources :teams,       only: [:index, :show, :create, :update, :edit]
+  resources :teams,       only: [:show, :create, :update, :edit]
   resources :flags,       only: [:index, :show, :create, :update, :edit]
   resources :sponsors,    only: [:index, :show, :create, :update, :edit]
   resources :people,      only: [:index, :show, :create, :update, :edit]
